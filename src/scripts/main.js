4e8f6dd5cbca1e812dc6a309fcd700448bfd21c4
PhilipeF <philipesferreiraa@gmail.com>
2024-07-03 10:57:33 -0300

    Feat: Atualizando conforme o exercício pede

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Jul 15, 2022 19:00:00");
+const dataDoEvento = new Date("Apri 04, 2025 19:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const constaAsHoras = setInterval(() => {
